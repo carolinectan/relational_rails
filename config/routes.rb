e618f4a7f78d12787e600949db9354ace43531e3
Marla Schulz <marlaa.schulz@gmail.com>
2021-07-06 15:43:09 -0600

    Create gardens index, routes, view

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/', to: 'welcome#index'
+
+  get '/gardens', to: 'gardens#index'
+  get '/farms', to: 'farms#index'
 end
