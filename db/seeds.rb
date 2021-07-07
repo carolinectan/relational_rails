be5d7feb25db1d1fc3270b4d25828e949b269e68
Marla Schulz <marlaa.schulz@gmail.com>
2021-07-07 11:38:26 -0600

    Add plants

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,6 +15,18 @@ miracle_garden = Garden.create!(name: 'Miracle Garden', irrigation: true, plant_
 butterfly_garden = Garden.create!(name: 'Butterfly Garden', irrigation: false, plant_capacity: 40)
 bonsai_garden = Garden.create!(name: 'Bonsai Garden', irrigation: true, plant_capacity: 10)
 
+miracle_garden.plants.create!(name: 'eggplant', edible: true, mature_height: 14)
+miracle_garden.plants.create!(name: 'tomato', edible: true, mature_height: 48)
+miracle_garden.plants.create!(name: 'cucumber', edible: true, mature_height: 30)
+
+butterfly_garden.plants.create!(name: 'aster', edible: false, mature_height: 50)
+butterfly_garden.plants.create!(name: 'bee balm', edible: true, mature_height: 24)
+butterfly_garden.plants.create!(name: 'lavender', edible: true, mature_height: 48)
+
+bonsai_garden.plants.create!(name: 'juniper bonsai', edible: true, mature_height: 16)
+bonsai_garden.plants.create!(name: 'satsuki azaliea', edible: false, mature_height: 72)
+bonsai_garden.plants.create!(name: 'chinese elm', edible: false, mature_height: 55)
+
 strawberry_farm = Farm.create!(name: 'Strawberry Farm', has_animals: false, num_of_fields: 22)
 llama_farm = Farm.create!(name: 'Llama Farm', has_animals: true, num_of_fields: 5)
 vegetable_farm = Farm.create!(name: 'Vegetable Farm', has_animals: false, num_of_fields: 68)
