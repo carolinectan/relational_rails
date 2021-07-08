d638e245b03e0453e5a8f65c736f85ecd61e7196
Marla Schulz <marlaa.schulz@gmail.com>
2021-07-08 13:48:38 -0600

    Add garden index link

--- spec/features/gardens/index_spec.rb
+++ spec/features/gardens/index_spec.rb
@@ -33,4 +33,18 @@ RSpec.describe 'gardens index page' do
 
     page.has_xpath?('/plants')
   end
+
+  it 'displays a link that takes you to the garden index' do
+    visit "/gardens"
+
+    garden1 = Garden.create!(name: 'last garden', irrigation: true, plant_capacity: 5)
+    garden2 = Garden.create!(name: 'middle garden', irrigation: false, plant_capacity: 6)
+    garden3 = Garden.create!(name: 'first garden', irrigation: true, plant_capacity: 10)
+
+    expect(page).to have_content('Garden Index')
+
+    click_on('Garden Index')
+
+    page.has_xpath?('/gardens')
+  end
 end
--- spec/features/gardens/show_spec.rb
+++ spec/features/gardens/show_spec.rb
@@ -1,40 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe 'gardens show page' do
+  before(:each) do
+    @garden = Garden.create!(name: 'sample garden', irrigation: true, plant_capacity: 10)
 
+    @plant1 = @garden.plants.create!(name: 'daisy', edible: false, mature_height: 10)
+    @plant2 = @garden.plants.create!(name: 'rose', edible: true, mature_height: 42)
+  end
   it 'displays garden name and attributes' do
+    visit "/gardens/#{@garden.id}"
 
-    garden = Garden.create!(name: 'sample garden', irrigation: true, plant_capacity: 10)
-
-    visit "/gardens/#{garden.id}"
-
-    expect(page).to have_content(garden.name)
-    expect(page).to have_content(garden.irrigation)
-    expect(page).to have_content(garden.plant_capacity)
+    expect(page).to have_content(@garden.name)
+    expect(page).to have_content(@garden.irrigation)
+    expect(page).to have_content(@garden.plant_capacity)
   end
 
   it 'displays the number of plants associated with the garden' do
-    garden = Garden.create!(name: 'sample garden', irrigation: true, plant_capacity: 10)
-
-    plant1 = garden.plants.create!(name: 'daisy', edible: false, mature_height: 10)
-    plant2 = garden.plants.create!(name: 'rose', edible: true, mature_height: 42)
-
-    visit "/gardens/#{garden.id}"
+    visit "/gardens/#{@garden.id}"
 
     expect(page).to have_content('Total Plants: 2')
   end
 
   it 'displays a link that takes you to the plant index' do
-    visit "/gardens"
-
-    garden1 = Garden.create!(name: 'last garden', irrigation: true, plant_capacity: 5)
-    garden2 = Garden.create!(name: 'middle garden', irrigation: false, plant_capacity: 6)
-    garden3 = Garden.create!(name: 'first garden', irrigation: true, plant_capacity: 10)
+    visit "/gardens/#{@garden.id}"
 
     expect(page).to have_content('Plant Index')
 
     click_on('Plant Index')
 
     page.has_xpath?('/plants')
+  end
+
+  it 'displays a link that takes you to the garden index' do
+    visit "/gardens/#{@garden.id}"
+
+    expect(page).to have_content('Garden Index')
+
+    click_on('Garden Index')
+
+    page.has_xpath?('/gardens')
   end
 end
