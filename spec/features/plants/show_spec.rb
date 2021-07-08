833b81d82df3fddd55b05b85e249640324554bd4
Marla Schulz <marlaa.schulz@gmail.com>
2021-07-08 13:54:34 -0600

    Add garden index link

--- spec/features/plants/show_spec.rb
+++ spec/features/plants/show_spec.rb
@@ -27,16 +27,28 @@ RSpec.describe 'plants show page' do
   end
 
   it 'displays a link that takes you to the plant index' do
-    visit "/gardens"
+    garden = Garden.create!(name: 'sample garden', irrigation: true, plant_capacity: 10)
+    plant1 = garden.plants.create!(name: 'daisy', edible: false, mature_height: 10)
 
-    garden1 = Garden.create!(name: 'last garden', irrigation: true, plant_capacity: 5)
-    garden2 = Garden.create!(name: 'middle garden', irrigation: false, plant_capacity: 6)
-    garden3 = Garden.create!(name: 'first garden', irrigation: true, plant_capacity: 10)
+    visit "/plants/#{plant1.id}"
 
     expect(page).to have_content('Plant Index')
 
     click_on('Plant Index')
 
     page.has_xpath?('/plants')
+  end
+
+  it 'displays a link that takes you to the garden index' do
+    garden = Garden.create!(name: 'sample garden', irrigation: true, plant_capacity: 10)
+    plant1 = garden.plants.create!(name: 'daisy', edible: false, mature_height: 10)
+    
+    visit "/plants/#{plant1.id}"
+
+    expect(page).to have_content('Garden Index')
+
+    click_on('Garden Index')
+
+    page.has_xpath?('/gardens')
   end
 end
