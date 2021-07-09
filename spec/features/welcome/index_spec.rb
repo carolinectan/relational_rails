aab5d7cee059e8ed093aaea685f49ba1cc7b1fa8
Caroline Tan <carolinectan3@gmail.com>
2021-07-09 01:13:57 -0400

    Add welcome index spec

---
+++ spec/features/welcome/index_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe 'welcome page index' do
+  it 'can display a navigation bar' do
+    visit '/'
+
+    expect(page).to have_link("Farm Index", :href => "/farms")
+    expect(page).to have_link("Farmer Index", :href => "/farmers")
+    expect(page).to have_link("Plants Index", :href => "/plants")
+    expect(page).to have_link("Garden Index", :href => "/gardens")
+  end
+end
