ee1bb6c6815a3fa2b8a071e46011b58a07dc5832
Caroline Tan <carolinectan3@gmail.com>
2021-07-09 00:48:48 -0400

    Add count of farmers per a farm

--- spec/features/farms/show_spec.rb
+++ spec/features/farms/show_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'farm show page', type: :feature do
-    it 'can display a specific farm and its attributes' do
+  it 'can display a specific farm and its attributes' do
     farm_1 = Farm.create!(name: 'Sheep Farm', has_animals: true, num_of_fields: 8)
     farm_2 = Farm.create!(name: 'Corn Farm', has_animals: false, num_of_fields: 15)
 
@@ -12,9 +12,26 @@ RSpec.describe 'farm show page', type: :feature do
     expect(page).to have_content("Number of Fields: #{farm_1.num_of_fields}")
 
     visit "/farms/#{farm_2.id}"
-    
+
     expect(page).to have_content(farm_2.name)
     expect(page).to have_content("Has Animals: #{farm_2.has_animals}")
     expect(page).to have_content("Number of Fields: #{farm_2.num_of_fields}")
+  end
+
+  it 'can display the number of children associated with a specific parent' do
+    farm_1 = Farm.create!(name: 'Carrot Farm', has_animals: false, num_of_fields: 8)
+    farm_2 = Farm.create!(name: 'Fish Farm', has_animals: true, num_of_fields: 15)
+
+    farm_1.farmers.create!(name: 'Sami', age: 35, owns_land: true)
+    farm_2.farmers.create!(name: 'Carina', age: 28, owns_land: true)
+    farm_2.farmers.create!(name: 'Scott', age: 37, owns_land: false)
+
+    visit "/farms/#{farm_1.id}"
+
+    expect(page).to have_content("Number of Farmers: #{farm_1.farmers.count}")
+
+    visit "/farms/#{farm_2.id}"
+
+    expect(page).to have_content("Number of Farmers: #{farm_2.farmers.count}")
   end
 end
