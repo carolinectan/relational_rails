1f5a5ca10ec0dedb6e07dace81dc4a1e1a0e7cd1
Caroline Tan <carolinectan3@gmail.com>
2021-07-10 11:11:50 -0400

    Refactor form; add params to create action

--- app/controllers/farms_controller.rb
+++ app/controllers/farms_controller.rb
@@ -11,7 +11,7 @@ class FarmsController < ApplicationController
   end
 
   def create
-    farm = Farm.create(name: params[:name])
+    farm = Farm.create!(name: params[:name], has_animals: params[:has_animals], num_of_fields: params[:num_of_fields])
     redirect_to "/farms"
   end
 end
