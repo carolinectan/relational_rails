01a3d5482f3dd673d54f522e279d90a26bf998f0
Caroline Tan <carolinectan3@gmail.com>
2021-07-10 18:53:23 -0400

    Add destroy tests, add view for destroy farm from index

--- spec/features/farms/destroy_spec.rb
+++ spec/features/farms/destroy_spec.rb
@@ -11,8 +11,18 @@ RSpec.describe 'destroying a farm' do
   end
 
   it 'can delete the farm from the index page' do
+    visit "/farms"
+
+    click_button "Delete #{@farm_1.name}"
+
+    expect(current_path).to eq('/farms')
+    expect(page).to_not have_content("#{@farm_1.name}")
+    expect(page).to have_content("#{@farm_2.name}")
+    expect(page).to have_content("#{@farm_3.name}")
+  end
+
+  it 'can delete the farm from the show page' do
     visit "/farms/#{@farm_1.id}"
-    # save_and_open_page
 
     click_button "Delete #{@farm_1.name}"
 
@@ -22,8 +32,3 @@ RSpec.describe 'destroying a farm' do
     expect(page).to have_content("#{@farm_3.name}")
   end
 end
-
-# When I click the link "Delete Parent"
-# Then a 'DELETE' request is sent to '/parents/:id',
-# the parent is deleted, and all child records are deleted
-# and I am redirected to the parent index page where I no longer see this parent
