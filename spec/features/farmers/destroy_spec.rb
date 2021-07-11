bb1fb7c4ab295e6fc37828c4a0f24125b5ae2be2
Caroline Tan <carolinectan3@gmail.com>
2021-07-11 00:36:54 -0400

    Add delete farmer from farm farmer page test

--- spec/features/farmers/destroy_spec.rb
+++ spec/features/farmers/destroy_spec.rb
@@ -19,11 +19,30 @@ RSpec.describe 'destroying a farmer' do
     expect(page).to have_content(@farmer_2.name)
     expect(page).to have_content(@farmer_3.name)
 
-    click_button "Delete #{@farmer_1.name}"
+    click_button "Delete Farmer #{@farmer_1.name}"
 
     expect(current_path).to eq('/farmers')
     expect(page).to_not have_content(@farmer_1.name)
     expect(page).to have_content(@farmer_2.name)
     expect(page).to have_content(@farmer_3.name)
   end
+
+  it 'can delete the farmer from the farm farmer page' do
+    visit "/farms/#{@farm.id}/farmers"
+
+    expect(page).to have_content("#{@farmer_1.name}\nAge: #{@farmer_1.age}\nOwns Land: #{@farmer_1.owns_land}")
+    expect(page).to have_content("#{@farmer_2.name}\nAge: #{@farmer_2.age}\nOwns Land: #{@farmer_2.owns_land}")
+    expect(page).to have_content("#{@farmer_3.name}\nAge: #{@farmer_3.age}\nOwns Land: #{@farmer_3.owns_land}")
+    save_and_open_page
+
+    click_button "Delete Farmer #{@farmer_1.name}"
+
+    expect(current_path).to eq('/farmers')
+    expect(page).to_not have_content("#{@farmer_1.name}\nAge: #{@farmer_1.age}\nOwns Land: #{@farmer_1.owns_land}")
+    # expect(page).to have_content("#{@farmer_2.name}\nAge: #{@farmer_2.age}\nOwns Land: #{@farmer_2.owns_land}")
+    # expect(page).to have_content("#{@farmer_3.name}\nAge: #{@farmer_3.age}\nOwns Land: #{@farmer_3.owns_land}")
+
+    save_and_open_page
+  end
+
 end
