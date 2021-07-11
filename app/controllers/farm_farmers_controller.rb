e6e9e18fdd39fd4d13b4fe68741a36aef075c6e3
Caroline Tan <carolinectan3@gmail.com>
2021-07-11 00:39:36 -0400

    Add farm farmers destroy action

--- app/controllers/farm_farmers_controller.rb
+++ app/controllers/farm_farmers_controller.rb
@@ -3,4 +3,10 @@ class FarmFarmersController < ApplicationController
     @farm = Farm.find(params[:id])
     @farmers = @farm.farmers
   end
+
+  def destroy
+    farm_farmer = Farmer.find(params[:id])
+    farm_farmer.destroy
+    redirect_to '/farmers'
+  end
 end
