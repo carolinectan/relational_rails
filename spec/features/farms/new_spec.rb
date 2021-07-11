dccc77d64cb77f4102c15f024ec0246ed78ed4ce
Caroline Tan <carolinectan3@gmail.com>
2021-07-11 00:57:55 -0400

    Refactor test and views

--- spec/features/farms/new_spec.rb
+++ spec/features/farms/new_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'the Farm creation' do
   it 'links to the new page from the artists index' do
     visit '/farms'
 
-    click_link('New Farm')
+    click_link('Create New Farm')
 
     expect(current_path).to eq('/farms/new')
   end
@@ -15,7 +15,7 @@ RSpec.describe 'the Farm creation' do
     fill_in('name', with: 'Coconut Tree Farm')
     fill_in('has_animals', with: false)
     fill_in('num_of_fields', with: '42')
-    click_button('Create Farm')
+    click_button('Create New Farm')
 
     expect(current_path).to eq("/farms")
     expect(page).to have_content("Coconut Tree Farm\n(Created at: #{Farm.last.created_at})")
