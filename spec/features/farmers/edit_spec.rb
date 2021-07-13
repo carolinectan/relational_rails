35aace8670f0c7b4697de1cd36748b4ead55186a
Marla Schulz <marlaa.schulz@gmail.com>
2021-07-13 17:16:54 -0600

    Add testing for 100% coverage

--- spec/features/farmers/edit_spec.rb
+++ spec/features/farmers/edit_spec.rb
@@ -13,6 +13,25 @@ RSpec.describe 'the Farmer edit' do
     @farmer_4 = @farm.farmers.create!(name: 'David', age: 31, owns_land: true)
   end
 
+  it 'can edit the farmer' do
+    visit "/farmers"
+
+    expect(page).to have_content(@farmer_1.name)
+    expect(page).to have_content(@farmer_1.age)
+    expect(page).to have_content(@farmer_1.owns_land)
+    click_button "Update Farmer #{@farmer_1.name}"
+
+    fill_in "name", with: "Peter"
+    fill_in "age", with: 42
+    page.check("owns_land")
+    click_button "Update Farmer #{@farmer_1.name}"
+
+    expect(current_path).to eq("/farmers/#{@farmer_1.id}")
+    expect(page).to have_content("Peter")
+    expect(page).to have_content("42")
+    expect(page).to have_content("true")
+  end
+
   it 'links to the edit page from the farmer index page' do
     visit "/farmers"
 
--- spec/models/farmer_spec.rb
+++ spec/models/farmer_spec.rb
@@ -2,4 +2,20 @@ require 'rails_helper'
 
 RSpec.describe Farmer, type: :model do
   it {should belong_to :farm}
+  before(:each) do
+    Farmer.destroy_all
+    Farm.destroy_all
+
+    @farm = Farm.create!(name: 'Alpaca Farm', has_animals: true, num_of_fields: 3)
+    @farmer_1 = @farm.farmers.create!(name: 'James', age: 56, owns_land: true)
+    @farmer_2 = @farm.farmers.create!(name: 'Becky', age: 20, owns_land: false)
+  end
+
+  it 'can filter farmers if owns_land is true' do
+    expect(Farmer.owns_land_filter).to eq([@farmer_1])
+  end
+
+  it 'can sort farmers by name' do
+    expect(Farmer.sort_name).to eq([@farmer_2, @farmer_1])
+  end
 end
